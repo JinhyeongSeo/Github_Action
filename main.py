8b7b9a0b44e2e0e6f8d85f5cb330782b83360fb1
JinhyeongSeo <jh4clover@naver.com>
2025-03-11 17:02:05 +0900

    crawling을 위한 코드 작성

---
+++ main.py
@@ -0,0 +1,18 @@
+import crawler
+import createDir as dir
+from datetime import datetime
+
+# 1. 사이트에서 내용 크롤링하기
+crawler.getTitle()
+crawler.getHref()
+crawler.getDesc()
+
+# 2. 이번달 디렉토리 없으면 생성
+date = datetime.today()
+month = date.strftime('%Y-%m')
+# print(date)
+dir.createDirectory(month)
+
+# 3. 내용 저장
+filename = date.strftime('%Y-%m-%d')
+crawler.saveText(month, filename)
